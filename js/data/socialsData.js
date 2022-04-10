d990e601ef486e07ec24e8d2b68ffb218920489b
officiumph <78411456+officiumph@users.noreply.github.com>
2022-04-10 19:27:00 +0800

    Add files via upload

---
+++ js/data/socialsData.js
@@ -0,0 +1,24 @@
+const socialsData = [
+    {
+        icon: 'facebook',
+        link: '#'
+    },
+    5485126598,
+    'asgd',
+    true,
+    {},
+    {
+        icon: '',
+        link: ''
+    },
+    {
+        icon: 'linkedin',
+        link: '#'
+    },
+    {
+        icon: 'rss',
+        link: '#'
+    },
+];
+
+export { socialsData }
